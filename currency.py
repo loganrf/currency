66cfaa79b336131d8231d05bbc336f00b70fe91f
Logan Fagg <loganrf01@gmail.com>
2020-04-06 14:53:41 -0500

    Mostly works

--- currency.py
+++ currency.py
@@ -1,13 +1,29 @@
-
-class currency():
-	"""docstring for currency"""
+def parseCode(codeString):
 	
-	def __init__(self, name, country, code, symbol, before=True):
-		self.name = name
-		self.country = country
-		self.code = code
-		self.symbol = symbol
-		self.before = before
+	findIndex = codeString.find(',')
+	if(findIndex!=-1):
+		indices = [findIndex]
+		while(findIndex!=-1):
+			findIndex = codeString.find(',',indices[-1]+1)
+			if(findIndex!=-1):
+				indices+=[findIndex]
 
-		
-		
+		codes = []
+		startIndex = 0
+		for i in range(len(indices)):
+			codes+=[codeString[startIndex:indices[i]]]
+			startIndex = indices[i]+1
+		codes+=[codeString[startIndex:]]
+
+		rstr = ''
+		for i in codes:
+			rstr+=chr(int(i))
+
+		return rstr
+
+	else:
+		return chr(int(codeString))
+	
+
+print(parseCode('1083,1074,1124'))
+print(parseCode('1083,1074'))
--- getTypes.py
+++ getTypes.py
@@ -5,11 +5,50 @@ import time
 from bs4 import BeautifulSoup
 import re
 
+def parseCode(codeString):
+	
+	findIndex = codeString.find(',')
+	if(findIndex!=-1):
+		indices = [findIndex]
+		while(findIndex!=-1):
+			findIndex = codeString.find(',',indices[-1]+1)
+			if(findIndex!=-1):
+				indices+=[findIndex]
+
+		codes = []
+		startIndex = 0
+		for i in range(len(indices)):
+			codes+=[codeString[startIndex:indices[i]]]
+			startIndex = indices[i]+1
+		codes+=[codeString[startIndex:]]
+
+		rstr = ''
+		for i in codes:
+			rstr+=chr(int(i))
+
+		return rstr
+
+	else:
+		return chr(int(codeString))
+
 
 url = "https://www.xe.com/symbols.php"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
 result = soup.findAll('tr')
-for i in result:
-	print(i.findAll('td')[2])
+
+numResults = len(result)-10
+
+countryName = []
+codes = []
+uni = []
+
+for i in range(numResults):
+	countryName+=[result[i+1].findAll('td')[0].text]
+	codes+=[result[i+1].findAll('td')[1].text]
+	uni+=[result[i+1].findAll('td')[5].text]
+
+for i in uni:
+	if(i!=''):
+		print(parseCode(i))
